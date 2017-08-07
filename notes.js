7cfc5a111ae84414b2a1f4e5092df8102fff8770
fares ABADOU <fares.abadou@gmail.com>
2017-08-07 12:49:11 +0100

    added sendtotrash

--- notes.js
+++ notes.js
@@ -1,6 +1,13 @@
 
 
 var notes = {
+  currentColor : '' ,
+  SetColor : function(el) {
+    this.currentColor = el.getAttribute('theme');
+    if (document.querySelector('i[current]')) document.querySelector('i[current]').removeAttribute('current');
+    el.setAttribute('current','');
+  },
+
 showForm : function(){
 // .addForm.showForm
 document.querySelector('.addForm').classList.add('showForm');
@@ -12,15 +19,17 @@ document.querySelector('.addForm').classList.remove('showForm');
 submitAdd : function(){
   var title = document.querySelector('[notetitle]').innerText ,
    text = document.querySelector('[name="note"]').value ,
-   color = 'amber';
+   color = this.currentColor,
    id = document.querySelectorAll('.note').length;
-   id++;
+   id++,
+   z = 999 + id;
 document.querySelector('[notetitle]').innerText = 'Titre';
 document.querySelector('[name="note"]').value = '';
 this.addNote(title,text,color,id);
+this.hideForm();
 } ,
 addNote : function (tt,tx,cl,id) {
-var N = '<div class="note " id="nt'+id+'"  style="z-index:1005"  ><i class="close fa fa-close" onclick="confirmRemove(this.parentNode)"></i><h2>'+tt+'</h2><p>'+tx+'</p></div>';
+var N = '<div class="note '+cl+'" id="nt'+id+'"  style="z-index:'+z+'"  ><i class="close fa fa-close" onclick="this.parentNode.remove()"></i><i class="remove fa fa-trash" onclick="confirmRemove(this.parentNode)"></i><h2>'+tt+'</h2><p>'+tx+'</p></div>';
 
 
 document.querySelector('.cont').insertAdjacentHTML('beforeend',N);
